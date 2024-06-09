d0153a241dc8c33aa0170b52e29612bd83689b81
gauravgangwarweb <gauravgangwar501@gmail.com>
2024-06-09 13:29:49 +0530

    Completed with Faq

---
+++ src/app/components/Faq.tsx
@@ -0,0 +1,42 @@
+const Faq: React.FC = () => {
+  return (
+    <section className="relative mt-[420px] md:mt-48">
+      <div className="px-2 md:px-32">
+        <p className="text-[#FF5555] text-lg font-medium uppercase text-center md:text-left">
+          Faq
+        </p>
+        <h4 className="text-4xl md:text-5xl font-bold text-center md:text-left">
+          Frequently Asked <br /> Questions
+        </h4>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-14">
+            <div className="bg-[#FF5555] text-white p-5 rounded-lg">
+                <h5 className="text-[28px] font-semibold">The Best Financial Accounting App Ever!</h5>
+                <p className="text-lg font-medium">“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+            </div>
+            <div className="p-5 rounded-lg border md:border-none">
+                <h5 className="text-[28px] font-semibold">The Best Financial Accounting App Ever!</h5>
+                <p className="text-lg font-medium">“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+            </div>
+            <div className="p-5 rounded-lg border md:border-none">
+                <h5 className="text-[28px] font-semibold">The Best Financial Accounting App Ever!</h5>
+                <p className="text-lg font-medium">“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+            </div>
+            <div className="bg-[#FF5555] text-white p-5 rounded-lg">
+                <h5 className="text-[28px] font-semibold">The Best Financial Accounting App Ever!</h5>
+                <p className="text-lg font-medium">“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+            </div>
+            <div className="bg-[#FF5555] text-white p-5 rounded-lg">
+                <h5 className="text-[28px] font-semibold">The Best Financial Accounting App Ever!</h5>
+                <p className="text-lg font-medium">“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+            </div>
+            <div className="p-5 rounded-lg border md:border-none">
+                <h5 className="text-[28px] font-semibold">The Best Financial Accounting App Ever!</h5>
+                <p className="text-lg font-medium">“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Faq;
